ea8dcf6ffcfb067c092ca9e3aeb154fce63cc8fd
Baris Tacirlioglu <baristacirlioglu@gmail.com>
2025-08-05 11:29:32 +0300

    Enhances navbar with branding, search, and cart UI
    Improves user experience by adding logo, category selection,
    search input, and shopping cart indicator to the navigation bar.
    Modernizes layout for better accessibility and visual appeal.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,44 @@
+import { LayoutGrid, Search, ShoppingCart } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="p-5 shadow-sm flex justify-between">
+        <div className="flex items-center gap-8">
+            <Link href={"/"}>
+              <Image
+              alt= 'logo'
+              src= "/logo.png"
+                width={130}
+                height={90}
+                className="cursor-pointer"
+              />
+            </Link>
+
+            <h2 className="hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200 cursor-pointer">
+                <LayoutGrid className="h-5 w-5"/> Category
+            </h2>
+
+            <div className="md:flex hidden gap-2 items-center border rounded-full p-2 px-5">
+                <Search className="h-5 w-5"/>
+                <input 
+                    className="outline-none"
+                    placeholder="Search"
+                />
+            </div>
+
+        </div>
+
+        <div className="flex items-center gap-5">
+            <h2 className="flex gap-2 items-center text-lg">
+                <ShoppingCart className="h-7 w-7" />
+                <span className="bg-green-600 text-white px-2 rounded-full">2</span>
+            </h2>
+
+        </div>
+
+    </div>
   )
 }
 export default Navbar
