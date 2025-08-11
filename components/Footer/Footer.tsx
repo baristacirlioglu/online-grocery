cd6c39e730e3685bae2291f12bcee606fe534b78
Baris Tacirlioglu <baristacirlioglu@gmail.com>
2025-08-11 12:55:19 +0300

    Adds navigation links and social icons to footer
    Improves site usability and engagement by introducing
    navigation links and social media icons to the footer.
    Integrates react-icons as a new dependency for scalable
    icon usage.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FaTwitter, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -11,6 +12,78 @@ const Footer = () => {
           &copy; {new Date().getFullYear()} Online Grocery. All rights reserved
           by Barış Tacirlioğlu
         </p>
+
+        <ul className="mt-6 flex flex-wrap gap-8 justify-center">
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              Carreers
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              History
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              Mission
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              Project
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-800 hover:text-gray-950 cursor-pointer">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <ul className="mt-6 flex flex-wrap gap-8 justify-center">
+          <li>
+            <a
+              href="https://github.com/baristacirlioglu"
+              className="cursor-pointer"
+            >
+              <FaGithub size={24} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/" className="cursor-pointer">
+              <FaTwitter size={24} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/" className="cursor-pointer">
+              <FaInstagram size={24} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/baristacirlioglu/"
+              className="cursor-pointer"
+            >
+              <FaLinkedin size={24} />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
